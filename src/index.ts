cfe3ba3542f6b540b1beedaca9d63ccdfcf1f06c
jasl <jasl9187@hotmail.com>
2021-07-21 16:46:38 +0800

    Initial add Khala

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,17 @@
 // Copyright 2017-2020 @polkadot/apps-config authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
+import khala from './khala';
+import khalaDev from './khala-dev';
 import poc3 from './phala-poc3';
 import poc4 from './phala-poc4';
 import dev from './phala-dev';
 
-
 // alphabetical
 export const typesChain = {
+  'Khala': khala,
+  'Khala Testnet': khalaDev,
+  'Khala Local Testnet': khalaDev,
   'Phala PoC-3': poc3,
   'Phala PoC-4': poc4,
   'Phala PoC-Next': dev,
@@ -28,4 +32,7 @@ export {
   dev as Dev,
   dev as latest,
   dev as Latest,
+
+  khala,
+  khalaDev
 }
---
+++ src/khala-dev.ts
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: Apache-2.0
+
+export default {
+  "ChainId": "u8",
+  "ResourceId": "[u8; 32]",
+  "TokenId": "u256",
+  "DepositNonce": "u64",
+}
