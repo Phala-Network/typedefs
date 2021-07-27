0d1520c909dde43f6c505609fd240f88c7d128bf
jasl <jasl9187@hotmail.com>
2021-07-27 18:28:34 +0800

    add missing chainbridge typedefs

--- src/khala-dev.ts
+++ src/khala-dev.ts
@@ -5,4 +5,17 @@ export default {
   "ResourceId": "[u8; 32]",
   "TokenId": "u256",
   "DepositNonce": "u64",
+  "ProposalStatus": {
+    "_enum": {
+      "Initiated": null,
+      "Approved": null,
+      "Rejected": null
+    }
+  },
+  "ProposalVotes": {
+    "votes_for": "Vec<AccountId>",
+    "votes_against": "Vec<AccountId>",
+    "status": "ProposalStatus",
+    "expiry": "BlockNumber"
+  },
 }
