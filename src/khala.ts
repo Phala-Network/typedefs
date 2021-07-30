94248a7b3b14e47d9bc2be90fe82dd43ff4805db
jasl <jasl9187@hotmail.com>
2021-07-31 06:03:25 +0800

    update khala typedefs

--- src/khala.ts
+++ src/khala.ts
@@ -1,5 +1,23 @@
 // SPDX-License-Identifier: Apache-2.0
 
 export default {
-
+  "Address": "MultiAddress",
+  "LookupSource": "MultiAddress",
+  "ChainId": "u8",
+  "ResourceId": "[u8; 32]",
+  "TokenId": "u256",
+  "DepositNonce": "u64",
+  "ProposalStatus": {
+    "_enum": {
+      "Initiated": null,
+      "Approved": null,
+      "Rejected": null
+    }
+  },
+  "ProposalVotes": {
+    "votes_for": "Vec<AccountId>",
+    "votes_against": "Vec<AccountId>",
+    "status": "ProposalStatus",
+    "expiry": "BlockNumber"
+  }
 }
