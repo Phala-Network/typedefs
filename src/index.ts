5066e21a9808a8e1dfcbf568546f1fa17780bca3
jasl <jasl9187@hotmail.com>
2021-09-18 02:43:57 +0800

    use versionedKhala by default

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ import versionedKhala from './versioned-khala';
 
 // alphabetical
 export const typesChain = {
-  'Khala': khala,
+  'Khala': versionedKhala,
   'Khala Testnet': khalaDev,
   'Khala Local Testnet': khalaDev,
   'Phala PoC-Next': phalaDev,
